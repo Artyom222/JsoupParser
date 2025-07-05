6b90debf3aef8f12fe4ed7e4994655c8756bca2d
APecherkin <pech.artyom2@yandex.ru>
2025-07-05 16:30:47 +0500

    Revert "parsing"
    This reverts commit cb4d9bc99763b7e0d20ba67ea124b2253c718f83.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,5 @@
 package org.example
 
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
-
 fun main() {
-    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/" +
-            "avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/")
-        .timeout(10000)
-        .get()
-
-    val quotes: Elements = doc.select(".sc-2aegk7-2.bzpNIu")
-
-    var i = 0
-    for (quote: Element in quotes) {
-        ++i
-        println("Цитатата № $i")
-        println(quote.text())
-    }
 
 }
